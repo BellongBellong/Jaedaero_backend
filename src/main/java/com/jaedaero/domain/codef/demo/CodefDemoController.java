2c9834b84e3eeeaa626874648a5f84744c1a83b4
youknowwgit <choiyh2001@naver.com>
2026-07-30 23:13:39 +0900

    🚨 Fix: CODEF 데모 및 Swagger 접근 경로 수정

--- src/main/java/com/jaedaero/domain/codef/demo/CodefDemoController.java
+++ src/main/java/com/jaedaero/domain/codef/demo/CodefDemoController.java
@@ -10,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /** Server-rendered local demonstration of CODEF login, account cards, and transaction history. */
+@Controller
 @RequestMapping("/codef-demo")
 public class CodefDemoController {
 
