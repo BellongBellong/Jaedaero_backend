6830087ec7a8680fc26be64df4aa71c5115510d7
YSH <yang5864@naver.com>
2026-08-11 22:47:49 +0900

    ✨ Feat: 적립식 투자 가이드, 적립식 투자 계획 Swagger용으로 수정

--- src/main/java/com/jaedaero/domain/investmentguidance/controller/InvestmentGuidanceController.java
+++ src/main/java/com/jaedaero/domain/investmentguidance/controller/InvestmentGuidanceController.java
@@ -7,6 +7,9 @@ import com.jaedaero.domain.investmentguidance.exception.InvestmentGuidanceErrorC
 import com.jaedaero.domain.investmentguidance.exception.InvestmentGuidanceException;
 import com.jaedaero.domain.investmentguidance.service.InvestmentGuidanceService;
 import com.jaedaero.domain.strategyapplication.dto.StrategyApplicationResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,34 +22,66 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/api/v1/investment-guidances")
+@Api(tags = "적립식 투자 가이드")
 @RequiredArgsConstructor
 public class InvestmentGuidanceController {
 
   private final InvestmentGuidanceService service;
 
   @GetMapping("/latest")
-  public ResponseEntity<InvestmentGuidanceResponse> getLatest(Authentication authentication) {
+  @ApiOperation(value = "최신 적립식 투자 가이드 조회")
+  @ApiImplicitParam(
+      name = "X-User-Id",
+      value = "개발 환경에서 사용할 목 데이터 사용자 ID",
+      required = true,
+      paramType = "header",
+      example = "1")
+  public ResponseEntity<InvestmentGuidanceResponse> getLatest(
+      @ApiIgnore Authentication authentication) {
     return ResponseEntity.ok(service.getLatest(authenticatedUserId(authentication)));
   }
 
   @GetMapping("/{guidanceId}")
+  @ApiOperation(value = "적립식 투자 가이드 상세 조회")
+  @ApiImplicitParam(
+      name = "X-User-Id",
+      value = "개발 환경에서 사용할 목 데이터 사용자 ID",
+      required = true,
+      paramType = "header",
+      example = "1")
   public ResponseEntity<InvestmentGuidanceDetailResponse> getDetail(
-      Authentication authentication, @PathVariable long guidanceId) {
+      @ApiIgnore Authentication authentication, @PathVariable long guidanceId) {
     return ResponseEntity.ok(service.getDetail(authenticatedUserId(authentication), guidanceId));
   }
 
   @PostMapping
-  public ResponseEntity<InvestmentGuidanceResponse> create(Authentication authentication) {
+  @ApiOperation(value = "적립식 투자 가이드 생성")
+  @ApiImplicitParam(
+      name = "X-User-Id",
+      value = "개발 환경에서 사용할 목 데이터 사용자 ID",
+      required = true,
+      paramType = "header",
+      example = "1")
+  public ResponseEntity<InvestmentGuidanceResponse> create(
+      @ApiIgnore Authentication authentication) {
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(service.create(authenticatedUserId(authentication)));
   }
 
   @PostMapping("/{guidanceId}/apply")
+  @ApiOperation(value = "적립식 투자 가이드 적용")
+  @ApiImplicitParam(
+      name = "X-User-Id",
+      value = "개발 환경에서 사용할 목 데이터 사용자 ID",
+      required = true,
+      paramType = "header",
+      example = "1")
   public ResponseEntity<StrategyApplicationResponse> apply(
-      Authentication authentication,
+      @ApiIgnore Authentication authentication,
       @PathVariable long guidanceId,
       @Valid @RequestBody InvestmentGuidanceApplyRequest request) {
     return ResponseEntity.status(HttpStatus.CREATED)
